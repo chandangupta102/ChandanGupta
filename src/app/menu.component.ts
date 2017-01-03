011e932a24392429f3718a433719649f97281c45
Chandan Gupta <chandan.gupta@factset.com>
2017-01-03 13:52:11 +0530

    feat(Unit):Angular Router Added with navigation

---
+++ src/app/menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'menu',
+    templateUrl: './templates/menu.component.html',
+    styleUrls: ['./css/menu.component.css']
+})
+
+export class MenuComponent {
+
+
+}
+
---
+++ src/app/simpleInterest.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'simple-interest',
+    templateUrl: './templates/simpleInterest.component.html',
+    styleUrls: ['./css/simpleInterest.component.css']
+})
+
+export class SimpleInterestComponent {
+
+
+}
